f0843391930dea3f7b962135d8d434d5f28aedce
虞四明 <13966931523@163.com>
2019-03-11 11:55:51 +0800

    简化代码

--- src/main/java/com/mall/util/BigDecimalUtil.java
+++ src/main/java/com/mall/util/BigDecimalUtil.java
@@ -10,32 +10,24 @@ import java.math.RoundingMode;
  * @date 2018/11/29 20:02
  */
 public class BigDecimalUtil {
+    // 构造器私有化，只让使用静态方法
     private BigDecimalUtil() {
     }
 
     public static BigDecimal add(double v1, double v2) {
-        BigDecimal b1 = new BigDecimal(Double.toString(v1));
-        BigDecimal b2 = new BigDecimal(Double.toString(v2));
-        return b1.add(b2);
+        return BigDecimal.valueOf(v1).add(BigDecimal.valueOf(v2));
     }
 
     public static BigDecimal sub(double v1, double v2) {
-        BigDecimal b1 = new BigDecimal(Double.toString(v1));
-        BigDecimal b2 = new BigDecimal(Double.toString(v2));
-        return b1.subtract(b2);
+        return BigDecimal.valueOf(v1).subtract(BigDecimal.valueOf(v2));
     }
 
     public static BigDecimal mul(double v1, double v2) {
-        BigDecimal b1 = new BigDecimal(Double.toString(v1));
-        BigDecimal b2 = new BigDecimal(Double.toString(v2));
-        return b1.multiply(b2);
+        return BigDecimal.valueOf(v1).multiply(BigDecimal.valueOf(v2));
     }
 
     public static BigDecimal div(double v1, double v2) {
-        BigDecimal b1 = new BigDecimal(Double.toString(v1));
-        BigDecimal b2 = new BigDecimal(Double.toString(v2));
-        // 四舍五入，保留两位小数
-        return b1.divide(b2, 2, RoundingMode.HALF_UP);
+        // 四舍五入
+        return BigDecimal.valueOf(v1).divide(BigDecimal.valueOf(v2), RoundingMode.HALF_UP);
     }
-
 }
